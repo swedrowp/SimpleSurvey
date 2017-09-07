1065243af8d8b807c1dc89f9d10ab32fdf53e7b3
swedrowp <piotr.swedrowski@roche.com>
2017-09-07 08:51:15 +0200

    table view controller added

---
+++ Simple Survey/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Simple Survey
+//
+//  Created by swedrowp on 07/09/2017.
+//  Copyright © 2017 SurveyMonkey Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
